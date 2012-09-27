73f3c5b54925d3ae370fc81f9a225813af92460d
Jorge Acereda <jacereda@gmail.com>
2012-09-27 19:58:16 +0200

    Initial version

---
+++ ffvorbis/decode.go
@@ -0,0 +1,96 @@
+// Copyright 2012 The ffvorbis-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package ffvorbis provides a wrapper around the vorbis codec in ffmpeg.
+package ffvorbis
+
+// #cgo LDFLAGS: -lavcodec -lavutil
+/*
+#include "libavcodec/avcodec.h"
+#include "libavutil/samplefmt.h"
+extern AVCodec ff_vorbis_decoder;
+*/
+import "C"
+
+import (
+	"log"
+	"time"
+	"unsafe"
+)
+
+func init() {
+	C.av_log_set_level(48)
+	C.avcodec_register(&C.ff_vorbis_decoder)
+	
+}
+
+type Decoder struct {
+	c    *C.AVCodec
+	cc   *C.AVCodecContext
+}
+
+func NewDecoder(data []byte) *Decoder {
+	var d Decoder
+	d.c = C.avcodec_find_decoder(C.AV_CODEC_ID_VORBIS)
+	d.cc = C.avcodec_alloc_context3(d.c)
+	d.cc.extradata = (*C.uint8_t)(&data[0])
+	d.cc.extradata_size = C.int(len(data))
+	C.avcodec_open2(d.cc, d.c, nil)
+	return &d
+}
+
+func aligned(x uintptr) uintptr {
+	return (x+255)&0xffffffffffffff00
+}
+
+func (d *Decoder) Decode(data []byte, timecode time.Duration) *Samples {
+	var pkt C.AVPacket
+	var fr C.AVFrame
+	var got C.int
+	C.avcodec_get_frame_defaults(&fr)
+	C.av_init_packet(&pkt)
+	dl := len(data)
+	pkt.data = (*C.uint8_t)(&data[0])
+	pkt.size = C.int(dl)
+	dec := C.avcodec_decode_audio4(d.cc, &fr, &got, &pkt) 
+	if dec < 0 {
+		log.Println("Unable to decode 1")
+		return nil
+	}
+	if dec != pkt.size {
+		log.Println("Partial decode")
+	}
+	if got == 0 {
+		return nil
+	}
+	sz := int(d.cc.channels * fr.nb_samples) * (map[int32]int{
+		C.AV_SAMPLE_FMT_S16: 2, 
+		C.AV_SAMPLE_FMT_FLT: 4,
+	}[d.cc.sample_fmt])
+	buf := make([]byte, sz)
+	copy(buf, ((*[192000]byte)(unsafe.Pointer(fr.data[0])))[0:sz])
+	if pkt.data != nil {
+		C.av_free_packet(&pkt)
+	}
+	afmt := map[int32]Format{
+		C.AV_SAMPLE_FMT_S16: Int16, 
+		C.AV_SAMPLE_FMT_FLT: Float32,
+	}
+	return &Samples{buf, timecode, uint(d.cc.channels), 
+			uint(d.cc.sample_rate), afmt[d.cc.sample_fmt]}
+}
+
+type Format uint
+const (
+	Int16 = iota
+	Float32
+)
+
+type Samples struct {
+	Data []byte
+	Timecode time.Duration
+	Channels uint
+	Frequency uint
+	Format Format
+}
