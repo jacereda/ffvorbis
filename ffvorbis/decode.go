dff531cc74f52a17eab0cf4e2fd351e3c1aa31a9
Jorge Acereda <jacereda@gmail.com>
2016-01-09 22:51:51 +0100

    Updated to latest libavcodec API

--- ffvorbis/decode.go
+++ ffvorbis/decode.go
@@ -6,9 +6,10 @@
 // Package ffvorbis provides a wrapper around the vorbis codec in ffmpeg.
 package ffvorbis
 
-// #cgo LDFLAGS: -lavcodec
+// #cgo LDFLAGS: -lavcodec -lavutil
 /*
 #include "libavcodec/avcodec.h"
+#include "libavutil/frame.h"
 #if LIBAVCODEC_VERSION_MAJOR == 53
 #define AV_CODEC_ID_VORBIS CODEC_ID_VORBIS
 #endif
@@ -21,6 +22,16 @@ static void convertS16(void * vd, const void * vs, int n) {
  int i;
  for (i = 0; i < n; i++) d[i] = scale * s[i];
 }
+
+
+static void convertFLTP(void * vd, const void * vs, int n, int nch) {
+ const float ** s = (const float**)vs;
+ float * d = (float*)vd;
+ int i, ch;
+ for (ch = 0; ch < nch; ch++)
+  for (i = 0; i < n; i++) d[i*nch+ch] = s[ch][i];
+}
+
 */
 import "C"
 
@@ -45,22 +56,24 @@ func NewDecoder(data []byte, channels, rate int) *Decoder {
 	d.cc.codec_type = C.AVMEDIA_TYPE_AUDIO
 	d.cc.sample_rate = C.int(rate)
 	d.cc.channels = C.int(channels)
+	log.Println("channels ", d.cc.channels, "rate ", d.cc.sample_rate)
 	d.cc.extradata = (*C.uint8_t)(&data[0])
 	d.cc.extradata_size = C.int(len(data))
-	d.cc.channels = 2
 	C.avcodec_open2(d.cc, d.c, nil)
 	return &d
 }
 
 func (d *Decoder) Decode(data []byte) []float32 {
 	var pkt C.AVPacket
-	var fr C.AVFrame
+	var fr *C.AVFrame
 	var got C.int
-	C.avcodec_get_frame_defaults(&fr)
+	fr = C.av_frame_alloc()
+	defer C.av_frame_free(&fr)
 	C.av_init_packet(&pkt)
+	defer C.av_packet_unref(&pkt)
 	pkt.data = (*C.uint8_t)(&data[0])
 	pkt.size = C.int(len(data))
-	dec := C.avcodec_decode_audio4(d.cc, &fr, &got, &pkt)
+	dec := C.avcodec_decode_audio4(d.cc, fr, &got, &pkt)
 	if dec < 0 {
 		log.Println("Unable to decode")
 		return nil
@@ -80,11 +93,10 @@ func (d *Decoder) Decode(data []byte) []float32 {
 		C.memcpy(dst, src, C.size_t(nvals*4))
 	case C.AV_SAMPLE_FMT_S16:
 		C.convertS16(dst, src, nvals)
+	case C.AV_SAMPLE_FMT_FLTP:
+		C.convertFLTP(dst, unsafe.Pointer(&fr.data[0]), nvals, d.cc.channels)
 	default:
-		log.Panic("Unsupported format")
-	}
-	if pkt.data != nil {
-		C.av_free_packet(&pkt)
+		log.Panic("Unsupported format ", d.cc.sample_fmt)
 	}
 	return buf
 }
